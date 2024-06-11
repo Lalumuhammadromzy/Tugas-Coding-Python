d2b90e5df56e11a422f584b10eea10ae916e8b9a
Lalu Muhammad Romzy <164850941+Lalumuhammadromzy@users.noreply.github.com>
2024-06-11 05:54:11 -0700

    Add files via upload
    Tree
    Tree adalah struktur data hierarkis yang terdiri dari node yang diatur dalam level. Setiap node memiliki satu atau lebih anak, dan node tanpa anak disebut daun. Struktur tree yang paling umum adalah binary tree, di mana setiap node memiliki paling banyak dua anak.

---
+++ tree.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=' ')
+        inorder(root.right)
+
+# Penggunaan
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+inorder(root)  # Output: 4 2 5 1 3
